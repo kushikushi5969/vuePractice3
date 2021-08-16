bedc6edbbf2b78e4dabe121b749d830b7dd11cce
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-17 01:46:59 +0900

    router作成

---
+++ spa-vuepractice/src/router.js
@@ -0,0 +1,11 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue"
+import Users from "./views/Users.vue";
+
+// プラグイン(グローバルな機能)を適用する
+Vue.use(Router)
+
+export default new Router({
+  routes: [{path: '/', component: Home}, {path: '/users', component: Users}]
+});
