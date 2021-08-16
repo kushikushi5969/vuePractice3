53755fe20c66928768016c50d70db7960388c1e9
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-17 02:38:25 +0900

    childrenでネスト

--- spa-vuepractice/src/router.js
+++ spa-vuepractice/src/router.js
@@ -2,11 +2,25 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue"
 import Users from "./views/Users.vue";
+import UsersPosts from "./views/UsersPosts.vue";
+import UsersProfile from "./views/UsersProfile.vue";
 
 // プラグイン(グローバルな機能)を適用する
 Vue.use(Router)
 
 export default new Router({
   mode: "history",
-  routes: [{path: '/', component: Home}, {path: '/users', component: Users}]
+  routes: [
+    { path: '/',
+      component: Home },
+
+    { path: '/users/:id',
+      component: Users,
+      props: true,
+      children: [
+        { path: "posts", component: UsersPosts },
+        { path: "profile", component: UsersProfile },
+      ]
+    }
+  ]
 });
--- spa-vuepractice/src/views/Users.vue
+++ spa-vuepractice/src/views/Users.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
     <h3>Users</h3>
+    <router-link to="/users/1">ユーザー１</router-link>
+    <router-link to="/users/2">ユーザー２</router-link>
+    <hr>
+    <h1>User No. {{ id }}</h1>
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+export default {
+  props: ["id"]
+}
+</script>
