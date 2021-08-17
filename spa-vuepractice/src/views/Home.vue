3083541e03888b4318095970d4a870d94f1dcc6e
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-17 23:07:38 +0900

    pathの記入

--- spa-vuepractice/src/views/Home.vue
+++ spa-vuepractice/src/views/Home.vue
@@ -9,7 +9,10 @@
 export default {
   methods: {
     toUsers() {
-      this.$router.push({path: "users"});
+      this.$router.push({
+        name: 'users-id-profile',
+        params: { id: 1 }
+      });
     }
   }
 }
--- spa-vuepractice/src/views/Users.vue
+++ spa-vuepractice/src/views/Users.vue
@@ -5,7 +5,8 @@
     <router-link to="/users/2">ユーザー２</router-link>
     <hr>
     <h1>User No. {{ id }}</h1>
-    <router-link :to="'/users/' +  (Number(id) + 1) + '/profile'">次のユーザー</router-link>
+    <!-- <router-link :to="'/users/' +  (Number(id) + 1) + '/profile'">次のユーザー</router-link> -->
+    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }, query: { lang: 'ja', page: 2} }">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
