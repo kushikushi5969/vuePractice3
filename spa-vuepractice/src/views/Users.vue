3df1a1dbf612e788d58df13ba7aa960826201696
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-17 22:58:30 +0900

    to属性

--- spa-vuepractice/src/views/Users.vue
+++ spa-vuepractice/src/views/Users.vue
@@ -5,6 +5,7 @@
     <router-link to="/users/2">ユーザー２</router-link>
     <hr>
     <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' +  (Number(id) + 1) + '/profile'">次のユーザー</router-link>
     <router-view></router-view>
   </div>
 </template>
