5c6ecd62916e17adf890ae23b04fe7092a6daa6c
kushikushi5969 <yuya.kushida1221@gmail.com>
2021-08-20 17:56:22 +0900

    機能毎にモジュールとして分割する

--- spa-vuepractice/src/store/index.js
+++ spa-vuepractice/src/store/index.js
@@ -1,39 +1,28 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import count from './modules/count';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count: 2,
     message: ""
   },
   getters: {
-    doubleCount: state => state.count * 2,
-    tripleCount: state => state.count * 3,
     message: state => state.message
   },
   // stateの変更はmutationsだけで行う。追跡しやすや・バグ防止のため
   mutations: {
-    increment(state, number) {
-      state.count += number;
-    },
-    decrement(state, number) {
-      state.count -= number;
-    },
     updateMessage(state, newMessage) {
       state.message = newMessage;
     }
   },
   actions: {
-    increment({ commit }, number) {
-      commit('increment', number);
-    },
-    decrement({ commit }, number) {
-      commit('decrement', number);
-    },
     updateMessage({ commit }, newMessage) {
       commit("updateMessage",newMessage);
     }
+  },
+  modules: {
+    count
   }
 })
